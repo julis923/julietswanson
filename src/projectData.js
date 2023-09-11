1455fb48ea86457fbc18bb91379b5b93a7d9fe1e
Juliet Swanson <julietswanson@Juliets-MBP.lan>
2023-09-11 12:26:53 -0400

    hashlink fix

--- src/projectData.js
+++ src/projectData.js
@@ -602,7 +602,7 @@ const projects = [
     hex: "#309e7f",
     bannerImg: railtimeBanner,
     bannerBtns: [
-      { text: "Case study", link: "#", classes: "" },
+      { text: "Case study", link: "case-study", classes: "" },
       {
         text: "View prototype",
         link: "https://www.figma.com/proto/JeuuUI5NRKJz277JDmTeuQ/Railtime-v2?page-id=0%3A1&type=design&node-id=1-2&viewport=406%2C314%2C0.12&t=ldkVssUCZtGLWPn1-1&scaling=min-zoom&starting-point-node-id=1%3A2&mode=design",
